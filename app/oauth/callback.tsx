044ddedb50438f423654e2b4194880fe4b7b0d01
Apavayan Sinha <info@apavayan.com>
2025-04-17 10:23:38 +0530

    feat: Layout update CallbackLoadingScreen with logo display

--- app/oauth/callback.tsx
+++ app/oauth/callback.tsx
@@ -1,12 +1,33 @@
 import { useTheme } from "@/contexts/ThemeContext";
 import React from "react";
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, Image, StyleSheet, View } from "react-native";
 
 export default function CallbackLoadingScreen() {
   const { theme } = useTheme();
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={styles.logoContainer}>
+        <Image
+          source={{ uri: "https://erp.awokeindia.com/files/logo-sq.png" }}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </View>
       <ActivityIndicator size={"large"} color={theme.brandColors.primary} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logoContainer: {
+    marginBottom: 24,
+    width: 120,
+    height: 120,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: "100%",
+    height: "100%",
+  },
+});
